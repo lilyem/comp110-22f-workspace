c73b7ce7f1bd3c85833983962ecf1fbb38e8d766
Lily Moore <lilyem@unc.edu>
2022-09-05 23:47:23 -0400

    new one shot worlde

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,5 +1,5 @@
-"""One shot Wordle"""
-__author__: str = "730573460"
+"""One shot Wordle."""
+__author__ = "730573460"
 
 secret_word: str = "python"
 guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
@@ -14,10 +14,10 @@ while len(guess) != len(secret_word):
 
 match_in_word: bool = False
 alternate_index: int = 0
-#The while loop below goes through each index of the guess to see
-#if it matches the index in the secret word, matches a different
-#index in the secret word, or is not present in the secret word
-#at all and prints a different color box for each result
+# The while loop below goes through each index of the guess to see
+# if it matches the index in the secret word, matches a different
+# index in the secret word, or is not present in the secret word
+# at all and prints a different color box for each result
 while index_of_guess < len(secret_word):
     if guess[index_of_guess] == secret_word[index_of_guess]:
         resulting_emojis = resulting_emojis + GREEN_BOX
@@ -32,7 +32,7 @@ while index_of_guess < len(secret_word):
         else:
             resulting_emojis = resulting_emojis + WHITE_BOX
     index_of_guess = index_of_guess + 1
-    #reset the variables
+    # reset the variables
     match_in_word = False
     alternate_index = 0
 print(resulting_emojis)
