d8899863e50048af0197d7de04130a7d8286e2b8
Lily Moore <lilyem@unc.edu>
2022-10-11 22:32:50 -0400

    dictionary progress ex07

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -1,8 +1,8 @@
-""""""
+"""Dictionary Functions for EX07."""
 __author__ = "730573460"
 
 
-def invert (original_dict: dict[str, str]) -> dict[str, str]:
+def invert(original_dict: dict[str, str]) -> dict[str, str]:
     """Switches the key with the values."""
     inverted_dict: dict[str, str] = {}
     for key in original_dict:
@@ -12,7 +12,32 @@ def invert (original_dict: dict[str, str]) -> dict[str, str]:
     return inverted_dict
 
 
-def favorite_color (a: dict[str, str]) -> str:
+def favorite_color(a: dict[str, str]) -> str:
     """Returns most common favorite color"""
     most_popular_color: str = ""
+    number_of_color: int = 0
+    color_count: dict[str, int] = {}
+    for key in a:
+        if a[key] in color_count:
+            color_count[a[key]] += 1
+        else:
+            color_count[a[key]] = 1
+    for color in color_count:
+        if color_count[color] > number_of_color:
+            number_of_color = color_count[color]
+            most_popular_color = color
+    return most_popular_color
+
+
+
     
+
+def count(a: list[str]) -> dict[str, int]:
+    """Count the number of times something appears in a list."""
+    return_dict: dict[str, int] = {}
+    for item in a:
+        if item in return_dict:
+            return_dict[item] += 1
+        else:
+            return_dict[item] = 1
+    return return_dict
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,23 +1,59 @@
-""""""
+"""Tests for Dictionary Functions EX07."""
 __author__ = "730573460"
 
 
-from dictionary import invert
+from dictionary import invert, favorite_color, count
 
 
-def test_invert() -> None:
+def test_invert_empty() -> None:
     """Edge case for invert."""
     xs: dict[str, str] = {}
-    assert invert(xs) == []
+    assert invert(xs) == {}
 
 
-def test_invert() -> None:
+def test_invert_letters() -> None:
     """First use case for invert."""
-    xs: dict[str, str] = {'a':'z', 'b':'y', 'c':'x'}
-    assert invert(xs) == {'z':'a', 'y':'b', 'x':'c'}
+    xs: dict[str, str] = {"a":"z", "b":"y", "c":"x"}
+    assert invert(xs) == {"z":"a", "y":"b", "x":"c"}
 
 
-def test_invert() -> None:
-    """Second use case for invert with repeat keys."""
-    xs: dict[str, str] = {'Kris':'jordan', 'micheal':'jordan'}
-    assert invert(xs) == "KeyError: duplicate keys."
+def test_invert_names() -> None:
+    """Second use case for invert."""
+    xs: dict[str, str] = {"Lily":"Moore", "Amelia":"Heide"}
+    assert invert(xs) == {"Moore":"Lily", "Heide":"Amelia"}
+
+
+def test_favorite_color_empty() -> None:
+    """Edge case for favorite color function."""
+    xs: dict[str, str] = {}
+    assert favorite_color(xs) == ""
+
+
+def test_favorite_color_one() -> None:
+    """Use case for favorite color function with one favorite color."""
+    xs: dict[str, str] = {"Lily":"blue", "Marc":"yellow", "Kris":"blue"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_favorite_color_multiple() -> None:
+    """Use case for favorite color function with multiple favorite colors."""
+    xs: dict[str, str] = {"kendall":"blue", "lily":"blue", "amelia":"purple", "Maci":"purple"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_count_empty() -> None:
+    """Edge case for count function."""
+    xs: list[str] = list()
+    assert count(xs) == {}
+
+
+def test_count_one() -> None:
+    """First use case test for count function."""
+    xs: list[str] = ["a", "a", "b", "c", "a", "b"]
+    assert count(xs) == {"a":3, "b":2, "c":1}
+
+
+def test_count_two() -> None:
+    """Second use case test for count function."""
+    xs: list[str] = ["lily", "kendall", "lily"]
+    assert count(xs) == {"lily":2, "kendall":1}
